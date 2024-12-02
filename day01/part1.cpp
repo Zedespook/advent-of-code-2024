c7a0d085ffd6e1866885bce81f1abafc0cb89f37
Zedespook <26501287+Zedespook@users.noreply.github.com>
2024-12-02 13:19:05 +0100

    style: Format day 1 files with clang

--- day01/part1.cpp
+++ day01/part1.cpp
@@ -3,26 +3,26 @@ using namespace std;
 using ll = long long;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    
-    vector<int> left, right;
-    int x, y;
-    
-    while (cin >> x >> y) {
-        left.push_back(x);
-        right.push_back(y);
-    }
-    
-    sort(left.begin(), left.end());
-    sort(right.begin(), right.end());
-    
-    ll total_distance = 0;
-    for (size_t i = 0; i < left.size(); i++) {
-        total_distance += abs(left[i] - right[i]);
-    }
-    
-    cout << total_distance << endl;
-    
-    return 0;
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  vector<int> left, right;
+  int x, y;
+
+  while (cin >> x >> y) {
+    left.push_back(x);
+    right.push_back(y);
+  }
+
+  sort(left.begin(), left.end());
+  sort(right.begin(), right.end());
+
+  ll total_distance = 0;
+  for (size_t i = 0; i < left.size(); i++) {
+    total_distance += abs(left[i] - right[i]);
+  }
+
+  cout << total_distance << endl;
+
+  return 0;
 }
